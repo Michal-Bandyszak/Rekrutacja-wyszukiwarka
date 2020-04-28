ef8166473198675d98d8cbf254c7a056eec96b7b
Michal Bandyszak <michalbandyszak@gmail.com>
2020-04-28 12:03:14 +0200

    Added env variable to store api key

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const APIKey = 'f6g7vgEAMS2OS3pAAvpnZxAILT3r4bAB'
+const APIKey = process.env.REACT_APP_APIKey
 export const url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKey}&limit=9&q=`
 
